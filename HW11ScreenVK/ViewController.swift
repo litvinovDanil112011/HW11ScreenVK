f1d262898a13d9b016ded6c157ababcefe0ddf87
litvinovDanil112011 <danil112011@mail.ru>
2022-05-23 22:35:42 +0300

    Initial Commit

---
+++ HW11ScreenVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW11ScreenVK
+//
+//  Created by Daniil Litvinov on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
